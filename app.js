436674e3f6a9279c5deb5a57f8798d81a9ec069f
Brice RABOUIN <brice@MacBook-Air-de-Brice.local>
2023-01-27 13:04:23 +0100

    final version

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const bulle = document.querySelector('.position');
+
+const burger = document.querySelector('.burger-menu');
+
+
+burger.addEventListener('click', function () {
+    bulle.classList.toggle("visible");
+});
+
